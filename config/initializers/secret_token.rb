83abfdc6cfe861434621703939978613e081efc3
Oliver Ponder <oliver.ponder@gmail.com>
2012-05-03 23:27:58 +0200

    Add rails and edit readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigGollum::Application.config.secret_token = 'a44c9acea0417e813e106c51537241a3c2985664277562ed08c35688f91d7ea6347124e4b6f9d3c2e4dcef4647de97c2c80dbd03aa5f3becfe83fafd24450a86'
