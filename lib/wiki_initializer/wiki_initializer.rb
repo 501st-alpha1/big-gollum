44ddba8e33589da12d3d3135b0d10c859a0592e3
Oliver Ponder <oliver.ponder@gmail.com>
2012-05-05 02:53:26 +0200

    Make the wikis folder if it is not there

--- lib/wiki_initializer/wiki_initializer.rb
+++ lib/wiki_initializer/wiki_initializer.rb
@@ -2,6 +2,10 @@ require 'grit'
 
 class WikiInitializer
   def self.create_wiki(wiki)
+    unless File.exists?(Rails.root.to_s + '/wikis/')
+      Dir::mkdir(Rails.root.to_s + '/wikis/')
+    end
+
     unless File.exists?(Rails.root.to_s + '/wikis/' + wiki.name)
       Dir::mkdir(Rails.root.to_s + '/wikis/' + wiki.name)
       `cd #{Rails.root.to_s + '/wikis/' + wiki.name}&&touch Home.md&&git init&&git add . &&git commit -a -m "Initial commit"`
