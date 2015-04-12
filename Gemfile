73284750e4a5afc785119dd03c411864c8ffa5e3
Oliver Ponder <oliver.ponder@gmail.com>
2015-04-12 15:20:05 +0200

    Updating and cleaning up
    - Removed crufty javascript and css.
    - Updated bootstrap and gollum.
    - No more weird monkey patching of gollum. Gollum is now just a required gem, and not shipped in the lib folder

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'haml'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -24,14 +26,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 gem 'devise'
-gem 'grit', "~> 2.4.1"
-gem 'github-markup'
-gem 'github-markdown'
-gem 'pygments.rb'
-gem 'posix-spawn'
-gem 'sinatra'
-gem 'mustache'
-gem 'mustache-sinatra'
-gem 'sanitize'
-gem 'nokogiri'
-gem "twitter-bootstrap-rails"
+gem 'grit'
+
+gem 'gollum'
--- lib/wiki_mounter/wiki_mounter.rb
+++ lib/wiki_mounter/wiki_mounter.rb
@@ -1,25 +1,58 @@
 class WikiMounter
   require 'rubygems'
-
-  # $path = gollum_path
+  require 'gollum/app'
 
   def self.call(env)
     wikis_root = "wikis/"
     wiki_name = env["action_dispatch.request.path_parameters"][:wiki]
     wiki_path = wikis_root + wiki_name
+    mount_path = "/wiki/" + wiki_name
 
-    @@mount_point = "/wiki/" + wiki_name
-    load Rails.root + 'lib/gollum/lib/gollum/frontend/app.rb'
-    Precious::App.set(:gollum_path, wiki_path)
-    Precious::App.set(:wiki_options, {})
-    if env['warden'].user && env['warden'].user.wikis.include?(Wiki.find_by_name(wiki_name))
-      Precious::App.call(env)
-    else
-      [404, {}, []]
+    gollums ||= {}
+    gollums[:wiki_name] ||= MapGollum.new(wiki_path, mount_path)
+    gollums[:wiki_name].call(env)
+  end
+
+end
+
+
+class MapGollum
+  def initialize(base_path, mount_path)
+    @mg = Rack::Builder.new do
+
+      gollum_path = File.expand_path(base_path) # CHANGE THIS TO POINT TO YOUR OWN WIKI REPO
+      wiki_options = {universal_toc: false, live_preview: false}
+      Precious::App.set(:gollum_path, gollum_path)
+      Precious::App.set(:default_markup, :markdown) # set your favorite markup language
+      Precious::App.set(:wiki_options, wiki_options)
+
+      map '/' do
+        run Proc.new { [302, { 'Location' => "http://localhost:3000#{mount_path}" }, []] }
+        # run Precious::App
+      end
+
+      map mount_path do
+        run Precious::App
+      end
     end
   end
 
-  def self.mount_point
-    @@mount_point
+  def call(env)
+    @mg.call(env)
   end
 end
+
+
+# wikis_root = "wikis/"
+# wiki_name = env["action_dispatch.request.path_parameters"][:wiki]
+# wiki_path = wikis_root + wiki_name
+
+# @@mount_point = "/wiki/" + wiki_name
+# load Rails.root + 'lib/gollum/lib/gollum/frontend/app.rb'
+# Precious::App.set(:gollum_path, wiki_path)
+# Precious::App.set(:wiki_options, {})
+# if env['warden'].user && env['warden'].user.wikis.include?(Wiki.find_by_name(wiki_name))
+#   Precious::App.call(env)
+# else
+#   [404, {}, []]
+# end
