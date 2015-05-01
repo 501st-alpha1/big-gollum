91594858a3738eca3b295324e0d101e12418c9af
Oliver Ponder <oliver.ponder@gmail.com>
2015-05-01 14:49:27 +0200

    Add secret_key_base to skip deprecation notice

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 BigGollum::Application.config.secret_token = 'a44c9acea0417e813e106c51537241a3c2985664277562ed08c35688f91d7ea6347124e4b6f9d3c2e4dcef4647de97c2c80dbd03aa5f3becfe83fafd24450a86'
+BigGollum::Application.config.secret_key_base = "e5a54e423cf0e3d6ada769afef40dff70e265f42d580cb75b168b7f8c4d8e026cac3890f6a9480a2a6ecfcf7b970c6a37888c1a087f632d28ac0e093a8103dd8"
