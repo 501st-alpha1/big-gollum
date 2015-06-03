253fc810cd1200d299075658d3b14650fb656595
Scott Weldon <scottw@scvdata.com>
2015-06-03 15:25:32 -0700

    Update syntax to fix deprecation warnings

--- features/step_definitions/wikis_steps.rb
+++ features/step_definitions/wikis_steps.rb
@@ -8,17 +8,17 @@ end
 
 Then /^I should see "([^"]*)" in the list of wikis$/ do |wiki_name|
   visit root_path
-  page.should have_content(wiki_name)
+  expect(page).to have_content(wiki_name)
 end
 
 Then /^I should not see "([^"]*)" in the list of wikis$/ do |wiki_name|
   visit root_path
-  page.should_not have_content(wiki_name)
+  expect(page).to_not have_content(wiki_name)
 end
 
 Then /^I should be a member of the "([^"]*)" wiki$/ do |wiki_name|
   wiki = Wiki.find_by_name(wiki_name)
-  wiki.users.include?(User.first).should == true
+  expect(wiki.users.include?(User.first)).to eq(true)
 end
 
 When /^when I go to the "([^"]*)" wiki$/ do |wiki_name|
@@ -27,7 +27,7 @@ When /^when I go to the "([^"]*)" wiki$/ do |wiki_name|
 end
 
 Then /^I should see "([^"]*)"$/ do |text|
-  page.should have_content text
+  expect(page).to have_content text
 end
 
 When /^I go to the list of wikis$/ do
@@ -39,9 +39,9 @@ When /^I delete the "([^"]*)" wiki$/ do |wiki_name|
 end
 
 Then /^I should not see "([^"]*)"$/ do |text|
-  page.should_not have_content text
+  expect(page).to_not have_content text
 end
 
 Then /^the "([^"]*)" wiki should not have a folder anymore$/ do |wiki_name|
-  File.exists?(Rails.root.to_s + '/wikis/' + wiki_name).should_not == true
+  expect(File.exists?(Rails.root.to_s + '/wikis/' + wiki_name)).to_not eq(true)
 end
