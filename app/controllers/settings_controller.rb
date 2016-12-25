2d9a5dc9d5fbb85e2bfe92029df78962c95e0738
Scott Weldon <open-source@scott-weldon.com>
2016-12-25 09:29:00 -0800

    Use loop to update all settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,15 +8,18 @@ class SettingsController < ApplicationController
   end
 
   def update
-    @setting = Setting.find_or_initialize_by(key: 'application_name')
+    ['application_name', 'allow_registrations'].each do |item|
+      setting = Setting.find_or_initialize_by(key: item)
+      input = resource_params[item]
 
-    if @setting.update_attributes(value: resource_params[:application_name][:value])
-      redirect_to settings_path, flash: {success: "Settings saved"}
-    else
-      flash.now[:error] = "Could not save settings"
-      @settings = load_settings
-      render :edit
+      if !setting.update_attributes(value: input[:value])
+        flash.now[:error] = "Could not save setting " + item
+        @settings = load_settings
+        render :edit
+      end
     end
+
+    redirect_to settings_path, flash: {success: "Settings saved"}
   end
 
   def resource_params
